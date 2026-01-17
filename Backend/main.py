f491bc276fcfd21a1e532d7003f02b09c02adafd
Daanish Vir <daanishv@student.cs.ubc.ca>
2026-01-17 15:02:26 -0800

    ADSF

---
+++ Backend/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from routes import tasks
+
+app = FastAPI()
+
+# app.include_router(projects.router)
+app.include_router(tasks.router)
+
+@app.get("/")
+def root():
+    return {"status": "Backend running"}
+
+
+    asdfasdf
